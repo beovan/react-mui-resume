3ed2faf50c6252008ff52ac4320e67bfb689bba8
root <truongvangiap04@gmail.com>
2024-03-12 13:56:43 +0700

    feat: axios and json-server dependencies, fetch and display posts

--- src/components/Develop.js
+++ src/components/Develop.js
@@ -1,5 +1,11 @@
+//React
+import { Box } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 
+//Axios
+// import Axios from './Axios/Axios';  
+import fetchPosts from './Posts/Posts';
+//local
 export const ButtonVisibilityContext = React.createContext();
 
 const Develop = () => {
@@ -14,18 +20,38 @@ const Develop = () => {
     const removeButton = () => {
         localStorage.removeItem('isButtonVisible');
     };
+//Axios
+const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+    fetchPosts().then(data => {
+        setPosts(data);
+    });
+}, []);
 
     return (
-        <ButtonVisibilityContext.Provider value={{ isButtonVisible, setButtonVisibility }}>
+        <Box>
+        <h1>Develop Option</h1>
+            <ButtonVisibilityContext.Provider value={{ isButtonVisible, setButtonVisibility }}>
             <div>Hello Again</div>
             {isButtonVisible && <button>Button</button>}
             <button onClick={() => setButtonVisibility(!isButtonVisible)}>
                 Toggle Button
             </button>
             <button onClick={removeButton}>
-                Remove from Local Storage
+                Hide Button
             </button>
         </ButtonVisibilityContext.Provider>
+        <h1>Posts</h1>
+        <div>
+        {posts.map((item) => (
+    <tr key={item.id}>
+        <td>{item.title}</td>
+        <td>{item.views} views</td>
+    </tr>
+))}
+    </div>
+        </Box>
     );
 }
 
