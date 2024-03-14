ef0cb342aa702de971ddac37fde13b6450611046
beovan <truongvangiap04@gmail.com>
2024-03-14 13:53:39 +0700

    feat: Update MongoDB URI and add bcrypt dependency

--- server.js
+++ server.js
@@ -1,31 +1,52 @@
-require('dotenv').config();
-//express
-const express = require('express');
-const app = express();
-//mongoose
+'use strict';
+
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+
+
+  User = require('./api/models/User'),
+  bodyParser = require('body-parser'),
+  jsonwebtoken = require("jsonwebtoken");
+
 const mongoose = require('mongoose');
+const option = {
+    socketTimeoutMS: 30000,
+    keepAlive: true,
+    reconnectTries: 30000
+};
 
-//Auth
-const cors = require('cors'); // Import cors module
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
-// const authRouter = require('./routes/auth');
-// Connect to database
-mongoose.connect(process.env.DATABASE_URI);
-const db = mongoose.connection
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect('mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb', option).then(function(){
+    //connected successfully
+}, function(err) {
+    //err handle
+});
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Middleware
-app.use(cors());
-// app.use('/auth', authRouter);
-app.use(express.json());
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+var routes = require('./api/routes/User/User');
+routes(app);
 
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+});
 
-// Routes
-const profileRouter = require('./api/routes/Profile');
-app.use('/Profiles', profileRouter);
+app.listen(port);
 
-// Start server
-app.listen(3000, () => console.log('Server started'));
+console.log(' RESTful API server started on: ' + port);
+
+module.exports = app;
